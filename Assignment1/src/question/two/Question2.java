eb74f8a70aae9a48f56b4cc671393c7255014d9b
D2-83689-Gaurav <gpatil873@gmail.com>
2024-06-18 18:36:26 +0530

    Question2 completed

---
+++ Assignment1/src/question/two/Question2.java
@@ -0,0 +1,41 @@
+package question.two;
+
+import java.util.Scanner;
+
+public class Question2 {
+	
+		public static int findLastOccurence(int []array , int key) {
+			int lastIndex = -1;
+			int comp = 0;
+			for(int i = 0 ; i< array.length; i++) {
+				comp++ ;				
+			if(key == array[i]) {
+				lastIndex = i;
+				break;
+				}
+			}
+			System.out.println("comp: "  +comp);
+			return lastIndex;
+					
+		}	
+
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int arr[] = {33,66,22,55,11,88,77,44,99 };
+			
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Enter key:");
+			int key = sc.nextInt();
+			
+			int index = findLastOccurence(arr ,key);
+			if (index!= -1)
+				System.out.println("last occurence of Key is at index :" +index  );
+			else
+				System.out.println("key is not found");
+			
+			
+			sc.close();
+		}
+		
+	}
