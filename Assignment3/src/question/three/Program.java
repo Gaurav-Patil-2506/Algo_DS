4d41f3c30ffd7694131a36d8a63562547fa5d234
D2-83689-Gaurav <gpatil873@gmail.com>
2024-06-24 14:28:57 +0530

    question complete

---
+++ Assignment3/src/question/three/Program.java
@@ -0,0 +1,16 @@
+package question.three;
+
+public class Program {
+public static void main(String[] args) {
+		
+		SLL ll = new SLL();
+		ll.addFirst(40);
+		ll.addFirst(30);
+		ll.addFirst(20);
+		ll.addFirst(10);
+		ll.display();
+		ll.DisplayReverse();
+
+	}
+
+}
