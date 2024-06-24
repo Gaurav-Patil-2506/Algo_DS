e31ddcddc0f2e00c4c650e7e7ad8886cab2c803b
D2-83689-Gaurav <gpatil873@gmail.com>
2024-06-24 23:06:52 +0530

    question2 complete

---
+++ Assignment5/src/question2/Program.java
@@ -0,0 +1,23 @@
+package question2;
+
+import question1.BST;
+
+public class Program {
+
+	public static void main(String[] args) {	
+
+	BST bst = new BST();
+		bst.addNodeRec(8);
+		bst.addNodeRec(3);
+		bst.addNodeRec(10);
+		bst.addNodeRec(6);
+		bst.addNodeRec(1);
+		bst.addNodeRec(14);
+		bst.addNodeRec(13);
+		bst.addNodeRec(7);
+		bst.addNodeRec(4);
+			
+		//bst.preorder();
+		
+	}
+}
