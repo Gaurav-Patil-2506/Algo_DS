b56f4a4c8b1746c0c0f29acf9d0af1af51f55cbe
D2-83689-Gaurav <gpatil873@gmail.com>
2024-06-24 14:32:08 +0530

    question complete

---
+++ Assignment3/src/question/four/Program.java
@@ -0,0 +1,16 @@
+package question.four;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SLL ll = new SLL();
+		ll.insertInSortedOrder(40);
+		ll.insertInSortedOrder(36);
+		ll.insertInSortedOrder(20);
+		ll.insertInSortedOrder(65);
+		
+		ll.display();
+	}
+
+}
